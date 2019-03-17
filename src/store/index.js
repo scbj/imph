c1e693424128c90aa5454ac8063d500d9ef5ecc0
Sacha Bejaud <sacha.bejaud@outlook.com>
2019-03-17 20:19:25 +0100

    Création des actions du store qui appellent l'API

--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import VuexPathify, { make } from 'vuex-pathify'
 
+import http from '@/http'
+
 Vue.use(Vuex)
 
 const state = {
@@ -10,7 +12,18 @@ const state = {
 }
 
 const actions = {
-
+  async listCategories ({ commit }) {
+    const { status, data } = await http.get('category')
+    if (status === 200 && data) {
+      commit('SET_CATEGORIES', data)
+    }
+  },
+  async listVideos ({ commit }, { categoryId }) {
+    const { status, data } = await http.get(`category/${categoryId}/video`)
+    if (status === 200 && data) {
+      commit('SET_VIDEOS', data)
+    }
+  }
 }
 
 const mutations = {
