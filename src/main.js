53ce1e6e6dab86cc1e8aa36ea75584700a15aa9a
Sacha Bejaud <sacha.bejaud@outlook.com>
2019-03-17 20:29:29 +0100

    Ajout de vue-mq

--- src/main.js
+++ src/main.js
@@ -1,9 +1,20 @@
 import Vue from 'vue'
+import VueMq from 'vue-mq'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
 Vue.config.productionTip = false
+
+Vue.use(VueMq, {
+  breakpoints: {
+    extraSmall: 576,
+    small: 768,
+    medium: 992,
+    large: 1200,
+    extraLarge: Infinity
+  }
+})
 
 new Vue({
   router,
