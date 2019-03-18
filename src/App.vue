b61b10c46bc11921a82c11be80b70b2f4e5a0419
Sacha Bejaud <sacha.bejaud@outlook.com>
2019-03-18 19:16:36 +0100

    Ajout de la police de caractère et des mixins

--- src/App.vue
+++ src/App.vue
@@ -7,10 +7,25 @@
 <style lang="scss">
 @import './assets/styles/_base.scss';
 
+$animation-easing: cubic-bezier(.165, .84, .44, 1);
+$animation-duration: .6s;
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: 'Roboto Mono', monospace;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  color: #000000;
+  color: #ffffff;
+  height: 100%;
+
+  animation:
+    darken-background
+    $animation-duration
+    $animation-easing
+    forwards;
+}
+
+// Animations
+@keyframes darken-background {
+  to { background: #000000 }
 }
 </style>
