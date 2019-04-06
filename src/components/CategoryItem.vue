a8bbde196df4a5fe385b628f760a10e1e5977215
Sacha Bejaud <sacha.bejaud@outlook.com>
2019-04-06 03:51:47 +0200

    Transition hover de <CategoryItem>
    ◽ Ajout de classe pour les éléments de <CategoryItem>
    ◽ Modification des tailles de <CategoryItem>
    ◽ Correction de l'agancement des élements dans <CategoryItem>

--- src/components/CategoryItem.vue
+++ src/components/CategoryItem.vue
@@ -1,9 +1,9 @@
 <template>
   <li :style="backgroundImage" class="category-item">
-    <h3>
+    <h2 class="label">
       {{ category.label | lowerCase }}
-    </h3>
-    <span>{{ videoCount + ' vidéos' }}</span>
+    </h2>
+    <span class="description">{{ videoCount + ' vidéos' }}</span>
   </li>
 </template>
 
@@ -37,9 +37,9 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-@import '../assets/styles/_vars.scss';
-@import '../assets/styles/_abstracts.scss';
-@import '../assets/styles/_fonts.scss';
+@import "../assets/styles/_vars.scss";
+@import "../assets/styles/_abstracts.scss";
+@import "../assets/styles/_fonts.scss";
 
 .category-item {
   // Background image properties
@@ -54,65 +54,81 @@ export default {
   display: inline-grid;
   grid-template-columns: 100%;
   grid-template-rows: 1fr auto 1fr;
-  justify-items: stretch;
+  justify-items: center;
   align-items: stretch;
   height: 16rem;
-  min-width: 22rem;
-  max-width: 30rem;
+  min-width: 21.5rem;
+  max-width: 28rem;
+  max-height: 15.4rem;
   // Position
   position: relative;
   overflow: hidden;
   // Effects
+  user-select: none;
+  cursor: pointer;
   box-shadow: 0px 5px 15px -5px #000000;
 
   @media screen and (min-width: 600px) {
     margin: $margin;
   }
+  @media screen and (min-width: $medium) {
+    opacity: 0.7;
+  }
 }
 
-h3 {
+.label {
   margin: 0;
   grid-row: 2/3;
   grid-column: 1/2;
-  text-shadow: 0 0px 55px #000000;
+  text-align: center;
+  text-shadow: 0 .04em .5em rgba(#000000, 20%);
 }
 
-span {
+.description {
   line-height: 0.5em;
   color: white;
-  opacity: 0;
   grid-row: 3/4;
   grid-column: 1/2;
-  transform: translateY(100px);
-  text-shadow: 0 0px 70px rgba(#000000, 50%);
+  transform: translateY(0);
+  text-shadow: 0 .04em .5em rgba(#000000, 30%);
+
+  @media screen and (min-width: $medium) {
+    opacity: 0;
+    transform: translateY(100px);
+  }
 }
 
-// Animations
-$easing: cubic-bezier(.215, .61, .355, 1);
+// Animations only on PC
+@media screen and (min-width: $medium) {
+  $easing: cubic-bezier(0.215, 0.61, 0.355, 1);
 
-// .category-item {
-//   transition:
-//     background .2s $easing,
-//     opacity .3s $easing,
-//     transform .3s $easing,
-//     box-shadow .3s $easing;
+  .category-item {
+    transition: background 0.2s $easing, opacity 0.3s $easing,
+    transform 0.3s $easing, box-shadow 0.3s $easing;
 
-//     a { transition: all .6s $easing .1s; }
-//     span { transition: all .3s $easing; }
+    &:hover {
+      background-size: 320%;
+      background-position: 40% 45%;
+      opacity: 1;
+      transform: translateY(-18px) scale(1.05);
+      box-shadow: 0px 15px 45px -10px rgba(#000000, 60%);
 
-//     &:hover {
-//       background-size: 320%;
-//       background-position: 40% 45%;
-//       opacity: 1;
-//       transform: translateY(-18px) scale(1.05);
-//       box-shadow: 0px 15px 45px -10px rgba(#000000, 60%);
+      .label {
+        transform: scale(1.05);
+      }
 
-//       a { transform: scale(1.05); }
+      .description {
+        opacity: 1;
+        transform: translateY(0);
+      }
+    }
+  }
 
-//       span {
-//         opacity: 1;
-//         transform: translateY(0);
-//       }
-//     }
-// }
+  .label {
+    transition: all 0.6s $easing 0.1s;
+  }
+  .description {
+    transition: all 0.3s $easing;
+  }
+}
 </style>
