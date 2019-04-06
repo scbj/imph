5f4005c4920622d1d6be754968bd81a936975086
Sacha Bejaud <sacha.bejaud@outlook.com>
2019-04-06 03:58:24 +0200

    Ajout des liens sociaux dans le footer

--- src/components/HomeFooter.vue
+++ src/components/HomeFooter.vue
@@ -1,19 +1,30 @@
 <template>
   <section class="home-footer">
+    <SocialLinks />
     <span class="copyright"><b>imphfilm</b> © 2019</span>
   </section>
 </template>
+
+<script>
+import SocialLinks from '@/components/SocialLinks.vue'
+
+export default {
+  components: { SocialLinks }
+}
+</script>
 
 <style lang="scss" scoped>
 .home-footer {
   background: black;
   padding: 2rem 0;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
 }
 
 .copyright {
+  margin-top: 2em;
   opacity: 0.87;
 }
 </style>
